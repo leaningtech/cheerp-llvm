744eff5c3a11a112f454f37228b59efee32db9db
Alessandro Pignotti <a.pignotti@sssup.it>
2012-07-08 15:48:46 +0200

    Some improvements toward support XMLHttpRequest fully in C++

--- tools/duetto/duetto.cpp
+++ tools/duetto/duetto.cpp
@@ -438,7 +438,7 @@ bool DuettoWriter::rewriteIfNativeConstructorCall(Module& M, Instruction* i, All
 	//Morph into a different call
 	//For some builtins we have special support. For the rest we use a default implementation
 	std::string duettoBuiltinCreateName;
-	if(builtinTypeName=="String")
+	if(builtinTypeName=="String" || builtinTypeName=="Array")
 		duettoBuiltinCreateName=builtinTypeName+"_duettoCreateBuiltin";
 	else
 		duettoBuiltinCreateName="default_duettoCreateBuiltin_"+builtinTypeName;
