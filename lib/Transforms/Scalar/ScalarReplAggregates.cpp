a9bb0b49698a85f1a7da5d72dbd19cbfcaceb82d
Alessandro Pignotti <a.pignotti@sssup.it>
2013-03-07 16:21:44 +0100

    Do not allow SROA on struct types
    NOTE: This is probably too eager, it would be useful to enable it on
    some cases

--- lib/Transforms/Scalar/ScalarReplAggregates.cpp
+++ lib/Transforms/Scalar/ScalarReplAggregates.cpp
@@ -2574,6 +2574,8 @@ static bool HasPadding(Type *Ty, const DataLayout &TD) {
 bool SROA::isSafeAllocaToScalarRepl(AllocaInst *AI) {
   // Loop over the use list of the alloca.  We can only transform it if all of
   // the users are safe to transform.
+  if(!TD->isByteAddressable() && AI->getAllocatedType()->isStructTy())
+    return false;
   AllocaInfo Info(AI);
 
   isSafeForScalarRepl(AI, 0, Info);
