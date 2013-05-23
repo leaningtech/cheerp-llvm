911c435c941a2146ca37d46db5533bd77d1f1f7e
Alessandro Pignotti <a.pignotti@sssup.it>
2013-05-23 20:12:35 +0200

    Add ByteAddressable flag to target

--- lib/IR/DataLayout.cpp
+++ lib/IR/DataLayout.cpp
@@ -182,6 +182,7 @@ void DataLayout::reset(StringRef Desc) {
   LayoutMap = nullptr;
   BigEndian = false;
   AllocaAddrSpace = 0;
+  ByteAddressable = true;
   StackNaturalAlign = 0;
   ProgramAddrSpace = 0;
   FunctionPtrAlign = 0;
@@ -267,6 +268,12 @@ void DataLayout::parseSpecifier(StringRef Desc) {
     case 's':
       // Ignored for backward compatibility.
       // FIXME: remove this on LLVM 4.0.
+      break;
+    case 'B':
+      ByteAddressable = true;
+      break;
+    case 'b':
+      ByteAddressable = false;
       break;
     case 'E':
       BigEndian = true;
