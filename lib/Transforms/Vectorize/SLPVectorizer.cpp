93c782d31289e001ed96ef0eec0201b5523d248f
Alessandro Pignotti <a.pignotti@sssup.it>
2013-08-23 17:05:33 +0200

    Make a few optimization conditional on byte addressable flag

--- lib/Transforms/Vectorize/SLPVectorizer.cpp
+++ lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -4619,6 +4619,9 @@ bool SLPVectorizerPass::runImpl(Function &F, ScalarEvolution *SE_,
   if (!TTI->getNumberOfRegisters(true))
     return false;
 
+  if (!DL->isByteAddressable())
+    return false;
+
   // Don't vectorize when the attribute NoImplicitFloat is used.
   if (F.hasFnAttribute(Attribute::NoImplicitFloat))
     return false;
