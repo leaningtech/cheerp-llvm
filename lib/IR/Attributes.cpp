406137399b0230d70711e7c3e53e6308423fd1e3
Alessandro Pignotti <a.pignotti@sssup.it>
2013-09-28 20:55:03 +0200

    Add an attribute for duetto casts

--- lib/IR/Attributes.cpp
+++ lib/IR/Attributes.cpp
@@ -395,6 +395,8 @@ std::string Attribute::getAsString(bool InAttrGrp) const {
     return "cold";
   if (hasAttribute(Attribute::ImmArg))
     return "immarg";
+  if (hasAttribute(Attribute::IsCast))
+    return "iscast";
 
   if (hasAttribute(Attribute::ByVal)) {
     std::string Result;
