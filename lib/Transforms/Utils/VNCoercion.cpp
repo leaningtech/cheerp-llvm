df33ada64efca94e7ac5954b3255c9c4c4af4d91
Alessandro Pignotti <a.pignotti@sssup.it>
2013-10-10 19:03:27 +0200

    Fix GVN on NBA targets

--- lib/Transforms/Utils/VNCoercion.cpp
+++ lib/Transforms/Utils/VNCoercion.cpp
@@ -14,6 +14,10 @@ namespace VNCoercion {
 /// Return true if coerceAvailableValueToLoadType will succeed.
 bool canCoerceMustAliasedValueToLoad(Value *StoredVal, Type *LoadTy,
                                      const DataLayout &DL) {
+  // Do not even try on NBA targets
+  if (!DL.isByteAddressable())
+    return false;
+
   Type *StoredTy = StoredVal->getType();
   if (StoredTy == LoadTy)
     return true;
@@ -164,6 +168,8 @@ static int analyzeLoadFromClobberingWrite(Type *LoadTy, Value *LoadPtr,
                                           Value *WritePtr,
                                           uint64_t WriteSizeInBits,
                                           const DataLayout &DL) {
+  if (!DL.isByteAddressable())
+    return -1;
   // If the loaded or stored value is a first class array or struct, don't try
   // to transform them.  We need to be able to bitcast to integer.
   if (LoadTy->isStructTy() || LoadTy->isArrayTy())
@@ -217,6 +223,8 @@ static int analyzeLoadFromClobberingWrite(Type *LoadTy, Value *LoadPtr,
 /// memdep query of a load that ends up being a clobbering store.
 int analyzeLoadFromClobberingStore(Type *LoadTy, Value *LoadPtr,
                                    StoreInst *DepSI, const DataLayout &DL) {
+  if (!DL.isByteAddressable())
+    return -1;
   auto *StoredVal = DepSI->getValueOperand();
   
   // Cannot handle reading from store of first-class aggregate yet.
@@ -245,6 +253,8 @@ int analyzeLoadFromClobberingStore(Type *LoadTy, Value *LoadPtr,
 /// the other load can feed into the second load.
 int analyzeLoadFromClobberingLoad(Type *LoadTy, Value *LoadPtr, LoadInst *DepLI,
                                   const DataLayout &DL) {
+  if (!DL.isByteAddressable())
+    return -1;
   // Cannot handle reading from store of first-class aggregate yet.
   if (DepLI->getType()->isStructTy() || DepLI->getType()->isArrayTy())
     return -1;
