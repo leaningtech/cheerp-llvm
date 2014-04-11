7b582385f6c85eabb8eeda7ffa28ac879fc1af3c
Alessandro Pignotti <a.pignotti@sssup.it>
2014-04-11 17:42:02 +0200

    Fix parsing of duetto attributes

--- lib/AsmParser/LLLexer.cpp
+++ lib/AsmParser/LLLexer.cpp
@@ -606,6 +606,8 @@ lltok::Kind LLLexer::LexIdentifier() {
   KEYWORD(sanitize_memory);
   KEYWORD(uwtable);
   KEYWORD(zeroext);
+  KEYWORD(client);
+  KEYWORD(server);
   KEYWORD(typed_intrinsic);
 
   KEYWORD(type);
