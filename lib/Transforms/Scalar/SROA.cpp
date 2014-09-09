df7e0ef3418fccb878cfab459895e225b9026555
Alessandro Pignotti <a.pignotti@sssup.it>
2014-09-09 17:33:03 +0200

    Improve SROA on NBA architectures

--- lib/Transforms/Scalar/SROA.cpp
+++ lib/Transforms/Scalar/SROA.cpp
@@ -659,11 +659,11 @@ class AllocaSlices::SliceBuilder : public PtrUseVisitor<SliceBuilder> {
   /// Set to de-duplicate dead instructions found in the use walk.
   SmallPtrSet<Instruction *, 4> VisitedDeadInsts;
 
-  Type* CastDestTy;
+  Type* AllocaTy;
 public:
   SliceBuilder(const DataLayout &DL, AllocaInst &AI, AllocaSlices &AS)
       : PtrUseVisitor<SliceBuilder>(DL),
-        AllocSize(DL.getTypeAllocSize(AI.getAllocatedType())), AS(AS), CastDestTy(NULL) {}
+        AllocSize(DL.getTypeAllocSize(AI.getAllocatedType())), AS(AS), AllocaTy(AI.getAllocatedType()) {}
 
 private:
   void markAsDead(Instruction &I) {
@@ -711,10 +711,8 @@ private:
     if (BC.use_empty())
       return markAsDead(BC);
 
-    if (!CastDestTy)
-      CastDestTy = BC.getType();
-    else if (CastDestTy != BC.getType() && !DL.isByteAddressable())
-      PI.setAborted(&BC);
+    if (!DL.isByteAddressable() && AllocaTy->isStructTy() && cast<StructType>(AllocaTy)->hasByteLayout())
+      return PI.setAborted(&BC);
     return Base::visitBitCastInst(BC);
   }
 
@@ -984,9 +982,9 @@ private:
       }
 
       if (BitCastInst* BC = dyn_cast<BitCastInst>(I)) {
-        if (!CastDestTy)
-          CastDestTy = BC->getType();
-        else if (CastDestTy != BC->getType() && !DL.isByteAddressable())
+        assert(UsedI->getType()->isPointerTy());
+        Type* UsedTy = UsedI->getType()->getPointerElementType();
+        if (!DL.isByteAddressable() && UsedTy->isStructTy() && cast<StructType>(UsedTy)->hasByteLayout())
           return BC;
       }
 
