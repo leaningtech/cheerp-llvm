8a04e16597ac65e60932d17756aa9297a3e880ae
Alessandro Pignotti <a.pignotti@sssup.it>
2015-01-27 09:31:11 +0100

    Never duplicate a block containing an heavy pointer

--- lib/Transforms/Scalar/JumpThreading.cpp
+++ lib/Transforms/Scalar/JumpThreading.cpp
@@ -327,6 +327,8 @@ static unsigned getJumpThreadDuplicationCost(BasicBlock *BB,
   // terminator-based Size adjustment at the end.
   Threshold += Bonus;
 
+  const DataLayout &DL = BB->getModule()->getDataLayout();
+
   // Sum up the cost of each instruction until we get to the terminator.  Don't
   // include the terminator because the copy won't include it.
   unsigned Size = 0;
@@ -338,6 +340,19 @@ static unsigned getJumpThreadDuplicationCost(BasicBlock *BB,
 
     // Debugger intrinsics don't incur code size.
     if (isa<DbgInfoIntrinsic>(I)) continue;
+
+    // On Cheerp do not ever duplicate a block which contains a pointer to an immutable type used outside of the block
+    if (!DL.isByteAddressable())
+    {
+      if (I->getType()->isPointerTy() && I->getType()->getPointerElementType()->isSingleValueType())
+      {
+        for (const User* U: I->users())
+        {
+          if (cast<Instruction>(U)->getParent() != BB)
+            return ~0U;
+        }
+      }
+    }
 
     // If this is a pointer->pointer bitcast, it is free.
     if (isa<BitCastInst>(I) && I->getType()->isPointerTy())
@@ -1067,6 +1082,10 @@ static bool isOpDefinedInBlock(Value *Op, BasicBlock *BB) {
 bool JumpThreadingPass::SimplifyPartiallyRedundantLoad(LoadInst *LI) {
   // Don't hack volatile and ordered loads.
   if (!LI->isUnordered()) return false;
+
+  const DataLayout &DL = LI->getModule()->getDataLayout();
+  if (!DL.isByteAddressable() && LI->getType()->isSingleValueType())
+    return false;
 
   // If the load is defined in a block with exactly one predecessor, it can't be
   // partially redundant.
