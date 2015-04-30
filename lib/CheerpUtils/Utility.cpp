a403f7c144d48ba902186d7b2e5ba9ca6daf3f24
Alessandro Pignotti <a.pignotti@sssup.it>
2015-04-30 17:19:18 +0200

    WIP TypeOptimizer

--- lib/CheerpUtils/Utility.cpp
+++ lib/CheerpUtils/Utility.cpp
@@ -635,6 +635,7 @@ void initializeCheerpOpts(PassRegistry &Registry)
 	initializeRegisterizePass(Registry);
 	initializeStructMemFuncLoweringPass(Registry);
 	initializeReplaceNopCastsPass(Registry);
+	initializeTypeOptimizerPass(Registry);
 }
 
 }
