7eb082dc110a1a5feff5efd9b8f155a941000228
Alessandro Pignotti <a.pignotti@sssup.it>
2015-04-30 17:12:24 +0200

    Make it possible to call ReplaceNopCasts from the command line

--- include/llvm/InitializePasses.h
+++ include/llvm/InitializePasses.h
@@ -426,6 +426,7 @@ void initializePointerAnalyzerPass(PassRegistry&);
 void initializeRegisterizePass(PassRegistry&);
 void initializeStructMemFuncLoweringPass(PassRegistry&);
 void initializeAllocaArraysPass(PassRegistry&);
+void initializeReplaceNopCastsPass(PassRegistry&);
 
 } // end namespace llvm
 
