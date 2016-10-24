aafad056dd0faf3cea1a7cdccb3e5af19e5616bb
Alessandro Pignotti <a.pignotti@sssup.it>
2016-10-24 11:26:15 +0200

    Avoid too much inlining when internalizing functions

--- lib/Analysis/InlineCost.cpp
+++ lib/Analysis/InlineCost.cpp
@@ -1885,7 +1885,7 @@ InlineResult CallAnalyzer::analyzeCall(CallSite CS) {
     }
   }
 
-  bool OnlyOneCallAndLocalLinkage =
+  bool OnlyOneCallAndLocalLinkage = DL.isByteAddressable() &&
       F.hasLocalLinkage() && F.hasOneUse() && &F == CS.getCalledFunction();
   // If this is a noduplicate call, we can still inline as long as
   // inlining this would cause the removal of the caller (so the instruction
