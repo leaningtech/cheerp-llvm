f93b31a4143695ad67a6f3478694879c459f6752
Alessandro Pignotti <a.pignotti@sssup.it>
2017-02-21 14:31:15 +0100

    BYTE_LAYOUT pointers that are converted to REGULAR count as escaping

--- include/llvm/Cheerp/Writer.h
+++ include/llvm/Cheerp/Writer.h
@@ -325,7 +325,7 @@ private:
 			valueKind == RAW)
 		{
 			stream << "{d:";
-			compilePointerBase(p, true);
+			compilePointerBase(p, kind != BYTE_LAYOUT);
 			stream << ",o:";
 			compilePointerOffset(p, LOWEST);
 			stream << "}";
