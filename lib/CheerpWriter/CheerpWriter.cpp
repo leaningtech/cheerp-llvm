4592d154847b80a1b362135aa24c5b01dd7b1761
Alessandro Pignotti <a.pignotti@sssup.it>
2017-03-04 19:34:38 +0100

    Avoid polluting types of a/i/d fields

--- lib/CheerpWriter/CheerpWriter.cpp
+++ lib/CheerpWriter/CheerpWriter.cpp
@@ -3271,7 +3271,7 @@ CheerpWriter::COMPILE_INSTRUCTION_FEEDBACK CheerpWriter::compileNotInlineableIns
 				{
 					compilePointerBase(ptrOp);
 					// Special support for client pointers, we cannot map them to integers because they would not be freed
-					stream << "['a'+(";
+					stream << "['l'+(";
 					compilePointerOffset(ptrOp, LOWEST);
 					stream << ")]=";
 					compileOperand(valOp);
@@ -4684,7 +4684,7 @@ CheerpWriter::COMPILE_INSTRUCTION_FEEDBACK CheerpWriter::compileInlineableInstru
 				{
 					compilePointerBase(ptrOp);
 					// Special support for client pointers, we cannot map them to integers because they would not be freed
-					stream << "['a'+(";
+					stream << "['l'+(";
 					compilePointerOffset(ptrOp, LOWEST);
 					stream << ")]";
 					return COMPILE_OK;
