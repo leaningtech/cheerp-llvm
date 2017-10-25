fde68093051b71380385c5d6107e654efd845465
Alessandro Pignotti <a.pignotti@sssup.it>
2017-10-25 11:53:26 +0200

    Allow tail calling the last void call in a function

--- lib/CheerpWriter/CheerpWriter.cpp
+++ lib/CheerpWriter/CheerpWriter.cpp
@@ -2973,7 +2973,8 @@ void CheerpWriter::compileMethodArgs(User::const_op_iterator it, User::const_op_
 		if(opCount)
 			stream << ',';
 		// Tail call support, directly pass the parent
-		if(isInlineable(*callV.getInstruction(), PA) && !callV.getInstruction()->getType()->isIntegerTy(1) && !currentLandingPad)
+		const llvm::Instruction* I = callV.getInstruction();
+		if((isInlineable(*I, PA) || isLastCallBeforeVoidReturn(*I)) && !I->getType()->isIntegerTy(1) && !currentLandingPad)
 			stream << 'p';
 		else
 			stream << 'a';
@@ -4550,7 +4551,7 @@ CheerpWriter::COMPILE_INSTRUCTION_FEEDBACK CheerpWriter::compileInlineableInstru
 								stream << '$';
 							else if(code[i] == 'a')
 							{
-								if(isInlineable(ci, PA) && !ci.getType()->isIntegerTy(1) && !currentLandingPad)
+								if((isInlineable(ci, PA) || isLastCallBeforeVoidReturn(ci)) && !ci.getType()->isIntegerTy(1) && !currentLandingPad)
 									stream << 'p';
 								else
 									stream << 'a';
@@ -4982,7 +4983,7 @@ void CheerpWriter::compileBB(const BasicBlock& BB, const std::set<uint32_t>& use
 			else if(II->getIntrinsicID()==Intrinsic::cheerp_downcast)
 				isDowncast = true;
 		}
-		if(needsStacklet && (isa<CallInst>(I) || isa<InvokeInst>(I)))
+		if(needsStacklet && (isa<CallInst>(I) || isa<InvokeInst>(I)) && !isLastCallBeforeVoidReturn(*I))
 		{
 			const llvm::Value* calledValue = isa<CallInst>(I) ? cast<CallInst>(I)->getCalledValue() : cast<InvokeInst>(I)->getCalledValue();
 			if(const InlineAsm* ia = dyn_cast<InlineAsm>(calledValue))
@@ -5353,6 +5354,8 @@ void CheerpWriter::compileMethodLocals(const Function& F, std::set<uint32_t>& us
 					assert(isa<ConstantInt>(I.getOperand(0)));
 					usedPCs.insert(cast<ConstantInt>(I.getOperand(0))->getZExtValue());
 				}
+				if(cheerp::isLastCallBeforeVoidReturn(I))
+					continue;
 				hasRecoveryPoint = true;
 			}
 		}
