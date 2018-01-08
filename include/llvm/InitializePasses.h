7dbb2cf5d9976a2949f4d6cc6513d2d419fcf65f
Alessandro Pignotti <a.pignotti@sssup.it>
2018-01-08 10:30:08 +0100

    Add I64Lowering pass

--- include/llvm/InitializePasses.h
+++ include/llvm/InitializePasses.h
@@ -439,6 +439,7 @@ void initializeFixIrreducibleControlFlowPass(PassRegistry&);
 void initializeCheerpLowerSwitchPass(PassRegistry&);
 void initializeFixFunctionCastsPass(PassRegistry&);
 void initializeByValLoweringPass(PassRegistry&);
+void initializeI64LoweringPass(PassRegistry&);
 
 } // end namespace llvm
 
