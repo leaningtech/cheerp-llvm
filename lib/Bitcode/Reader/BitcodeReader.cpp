b060b82bbe07692c660165ec1835f1b6dcf0997c
Yuri Iozzelli <y.iozzelli@gmail.com>
2018-04-10 10:38:55 +0200

    Fix reading the asmjs and bytelayaout flags from bitcode on opaque types

--- lib/Bitcode/Reader/BitcodeReader.cpp
+++ lib/Bitcode/Reader/BitcodeReader.cpp
@@ -1060,20 +1060,27 @@ std::error_code BitcodeReader::ParseTypeTableBody() {
       ResultTy = Res;
       break;
     }
-    case bitc::TYPE_CODE_OPAQUE: {       // OPAQUE: []
+    case bitc::TYPE_CODE_OPAQUE: {       // OPAQUE: [bytelayout, asmjs]
       if (Record.size() != 3)
         return Error("Invalid record");
 
       if (NumRecords >= TypeList.size())
         return Error("Invalid TYPE table");
 
+      bool hasByteLayout = Record[1];
+      bool hasAsmJS = Record[2];
       // Check to see if this was forward referenced, if so fill in the temp.
       StructType *Res = cast_or_null<StructType>(TypeList[NumRecords]);
       if (Res) {
         Res->setName(TypeName);
         TypeList[NumRecords] = nullptr;
-      } else  // Otherwise, create a new struct with no body.
+      } else {  // Otherwise, create a new struct with no body.
         Res = createIdentifiedStructType(Context, TypeName);
+        if (hasByteLayout)
+          Res->setByteLayout();
+        if (hasAsmJS)
+          Res->setAsmJS();
+      }
       TypeName.clear();
       ResultTy = Res;
       break;
