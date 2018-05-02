96b48d274f3475b102eab670f1039bcac49c9fd3
Alessandro Pignotti <a.pignotti@sssup.it>
2018-05-02 10:49:42 +0200

    Introduce a pass to collapse stores immediately after an alloca into the emitted literal

--- include/llvm/InitializePasses.h
+++ include/llvm/InitializePasses.h
@@ -434,6 +434,7 @@ void initializePreExecutePass(PassRegistry&);
 void initializeExpandStructRegsPass(PassRegistry&);
 void initializeFreeAndDeleteRemovalPass(PassRegistry&);
 void initializeGEPOptimizerPass(PassRegistry&);
+void initializeAllocaStoresExtractorPass(PassRegistry&);
 
 } // end namespace llvm
 
