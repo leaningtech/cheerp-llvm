5f315f0b799e64448d3b03372ca2f50f222db1f6
Yuri Iozzelli <y.iozzelli@gmail.com>
2018-08-13 14:28:47 +0200

    Add a pass to transform irreducible control flow to reducible

--- lib/Target/CheerpBackend/CheerpBackend.cpp
+++ lib/Target/CheerpBackend/CheerpBackend.cpp
@@ -24,6 +24,7 @@
 #include "llvm/Cheerp/AllocaMerging.h"
 #include "llvm/Cheerp/AllocaLowering.h"
 #include "llvm/Cheerp/AllocateArrayLowering.h"
+#include "llvm/Cheerp/FixIrreducibleControlFlow.h"
 #include "llvm/Cheerp/IdenticalCodeFolding.h"
 #include "llvm/Cheerp/PointerPasses.h"
 #include "llvm/Cheerp/CFGPasses.h"
@@ -133,6 +134,7 @@ bool CheerpTargetMachine::addPassesToEmitFile(PassManagerBase &PM,
   PM.add(createResolveAliasesPass());
   PM.add(createFreeAndDeleteRemovalPass());
   PM.add(cheerp::createGlobalDepsAnalyzerPass());
+  PM.add(createFixIrreducibleControlFlowPass());
   if (!CheerpNoICF)
     PM.add(cheerp::createIdenticalCodeFoldingPass());
   PM.add(createPointerArithmeticToArrayIndexingPass());
