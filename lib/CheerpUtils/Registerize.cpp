4bf1d1f022875125ddfea0b9c4a181d7117bc6e4
Alessandro Pignotti <a.pignotti@sssup.it>
2019-01-18 10:38:05 +0000

    Registerize: Handle pure-forwarding insts

--- lib/CheerpUtils/Registerize.cpp
+++ lib/CheerpUtils/Registerize.cpp
@@ -536,9 +536,10 @@ void Registerize::handlePHI(const Instruction& I, const LiveRangesTy& liveRanges
 		// If one of the operands already has a register allocated try to use that register again
 		for(Value* op: I.operands())
 		{
-			Instruction* usedI=dyn_cast<Instruction>(op);
-			if(!usedI || isInlineable(*usedI, PA))
+			const Instruction* usedI=getUniqueIncomingInst(op, PA);
+			if(!usedI)
 				continue;
+			assert(!isInlineable(*usedI, PA));
 			assert(liveRanges.count(usedI));
 			if(registersMap.count(usedI)==0)
 				continue;
@@ -559,9 +560,10 @@ void Registerize::handlePHI(const Instruction& I, const LiveRangesTy& liveRanges
 	// Iterate again on the operands and try to map as many as possible into the same register
 	for(Value* op: I.operands())
 	{
-		Instruction* usedI=dyn_cast<Instruction>(op);
-		if(!usedI || isInlineable(*usedI, PA))
+		const Instruction* usedI=getUniqueIncomingInst(op, PA);
+		if(!usedI)
 			continue;
+		assert(!isInlineable(*usedI, PA));
 		assert(liveRanges.count(usedI));
 		// Skip already assigned operands
 		if(registersMap.count(usedI))
