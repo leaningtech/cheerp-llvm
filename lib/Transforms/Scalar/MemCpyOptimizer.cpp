09b315632d82ec35b6897056c1530551b2c8f0e9
Alessandro Pignotti <a.pignotti@sssup.it>
2019-02-20 22:46:31 +0100

    Fix type safety on newly introduced memory intrinsics optimizations

--- lib/Transforms/Scalar/MemCpyOptimizer.cpp
+++ lib/Transforms/Scalar/MemCpyOptimizer.cpp
@@ -480,7 +480,8 @@ Instruction *MemCpyOptPass::tryMergingIntoMemset(Instruction *StartInst,
     }
 
     AMemSet =
-      Builder.CreateMemSet(StartPtr, ByteVal, Range.End-Range.Start, Alignment);
+      Builder.CreateMemSet(StartPtr, ByteVal, Range.End-Range.Start, Alignment,
+                                   false, NULL, NULL, NULL, DL.isByteAddressable());
 
     LLVM_DEBUG(dbgs() << "Replace stores:\n"; for (Instruction *SI
                                                    : Range.TheStores) dbgs()
@@ -1136,10 +1137,12 @@ bool MemCpyOptPass::processMemSetMemCpyDependence(MemCpyInst *MemCpy,
   Value *SizeDiff = Builder.CreateSub(DestSize, SrcSize);
   Value *MemsetLen = Builder.CreateSelect(
       Ule, ConstantInt::getNullValue(DestSize->getType()), SizeDiff);
+  const DataLayout &DL = MemCpy->getModule()->getDataLayout();
   Builder.CreateMemSet(
       Builder.CreateGEP(Dest->getType()->getPointerElementType(), Dest,
                         SrcSize),
-      MemSet->getOperand(1), MemsetLen, Align);
+      MemSet->getOperand(1), MemsetLen, Align,
+                       false, NULL, NULL, NULL, DL.isByteAddressable());
 
   MD->removeInstruction(MemSet);
   MemSet->eraseFromParent();
@@ -1207,8 +1210,10 @@ bool MemCpyOptPass::performMemCpyToMemSetOptzn(MemCpyInst *MemCpy,
   }
 
   IRBuilder<> Builder(MemCpy);
+  const DataLayout &DL = MemCpy->getModule()->getDataLayout();
   Builder.CreateMemSet(MemCpy->getRawDest(), MemSet->getOperand(1),
-                       CopySize, MemCpy->getDestAlignment());
+                       CopySize, MemCpy->getDestAlignment(),
+                       false, NULL, NULL, NULL, DL.isByteAddressable());
   return true;
 }
 
