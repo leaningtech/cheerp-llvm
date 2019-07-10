73a4ff4438267b62e8cb6698100116131f087844
Yuri Iozzelli <y.iozzelli@gmail.com>
2019-07-10 12:10:13 +0200

    Add ByValLowerint pass to correctly handle byval function parameters
    They get lowered in an alloca and a memcpy in the caller

--- include/llvm/InitializePasses.h
+++ include/llvm/InitializePasses.h
@@ -431,6 +431,7 @@ void initializeAllocaStoresExtractorPass(PassRegistry&);
 void initializeFixIrreducibleControlFlowPass(PassRegistry&);
 void initializeCheerpLowerSwitchPass(PassRegistry&);
 void initializeFixFunctionCastsPass(PassRegistry&);
+void initializeByValLoweringPass(PassRegistry&);
 
 } // end namespace llvm
 
