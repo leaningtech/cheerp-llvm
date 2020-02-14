8a4bb3cdb65f9ef1ac632c18c20ae0d786272c57
Alessandro Pignotti <a.pignotti@sssup.it>
2020-02-14 09:20:13 +0000

    Run StructMemFuncLowering only once in the LTO phase
    It is efficient to keep the operations as intrinsics in each translation
    unit, the lowering is done during LTO in a suitable position in the
    pipeline to further clean up generated loops

--- lib/Transforms/IPO/PassManagerBuilder.cpp
+++ lib/Transforms/IPO/PassManagerBuilder.cpp
@@ -23,6 +23,7 @@
 #include "llvm/Analysis/ScopedNoAliasAA.h"
 #include "llvm/Analysis/TargetLibraryInfo.h"
 #include "llvm/Analysis/TypeBasedAliasAnalysis.h"
+#include "llvm/Cheerp/StructMemFuncLowering.h"
 #include "llvm/IR/DataLayout.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/IR/Verifier.h"
@@ -146,6 +147,9 @@ cl::opt<bool> FlattenedProfileUsed(
 cl::opt<bool> EnableOrderFileInstrumentation(
     "enable-order-file-instrumentation", cl::init(false), cl::Hidden,
     cl::desc("Enable order file instrumentation (default = off)"));
+
+static cl::opt<bool> CheerpLTO("cheerp-lto", cl::init(false), cl::Hidden,
+                               cl::desc("Run passes needed for Cheerp LTO phase"));
 
 PassManagerBuilder::PassManagerBuilder() {
     OptLevel = 2;
@@ -391,6 +395,8 @@ void PassManagerBuilder::addFunctionSimplificationPasses(
                    : createGVNPass(DisableGVNLoadPRE)); // Remove redundancies
   }
   MPM.add(createMemCpyOptPass());             // Remove memcpy / form memset
+  if (CheerpLTO)
+    MPM.add(createStructMemFuncLowering());
   MPM.add(createSCCPPass());                  // Constant prop with SCCP
 
   // Delete dead bit computations (instcombine runs after to fold away the dead
