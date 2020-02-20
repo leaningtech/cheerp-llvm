e3f896c4b72700b21713d63444598dd0a09f9ca9
Alessandro Pignotti <a.pignotti@sssup.it>
2020-02-20 10:31:28 +0100

    SROA: Fix type-safety of memcpy creation when dealing with directbases

--- lib/Transforms/Scalar/SROA.cpp
+++ lib/Transforms/Scalar/SROA.cpp
@@ -1681,9 +1681,19 @@ static Value *getAdjustedPtr(IRBuilderTy &IRB, const DataLayout &DL, Value *Ptr,
   Ptr = OffsetPtr;
 
   // On the off chance we were targeting i8*, guard the bitcast here.
-  if (Ptr->getType() != PointerTy)
-  {
-    // Cheerp: We don't accept a unsafe cast, unless we are in asm.js mode
+  if (Ptr->getType() != PointerTy) {
+    Type* targetType = PointerTy->getPointerElementType();
+    // Cheerp: Only allow safe casts to direct bases, unless we are in asm.js mode
+    if(StructType* StTy = dyn_cast<StructType>(Ptr->getType()->getPointerElementType())) {
+      while(StructType* directBase = StTy->getDirectBase()) {
+        if(directBase == targetType) {
+          // The target type is a directbase of the current one, we can create a bitcast
+          Ptr = IRB.CreateBitCast(Ptr, PointerTy);
+          return Ptr;
+        }
+        StTy = directBase;
+      }
+    }
     if(!DL.isByteAddressable() && IRB.GetInsertBlock()->getParent()->getSection() != StringRef("asmjs"))
       return NULL;
     Ptr = IRB.CreateBitCast(Ptr, PointerTy, NamePrefix + "sroa_cast");
@@ -2936,7 +2946,7 @@ private:
     bool IsDest = &II.getRawDestUse() == OldUse;
     assert((IsDest && II.getRawDest() == OldPtr) ||
            (!IsDest && II.getRawSource() == OldPtr));
-    Type *RealPtrTy = OldPtr->stripPointerCastsSafe()->getType();
+    Type *RealPtrTy = DL.isByteAddressable() ? OldPtr->stripPointerCastsSafe()->getType() : OldPtr->getType();
 
     unsigned SliceAlign = getSliceAlign();
 
