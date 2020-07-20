35b3570c539d3ca625bca38f203db1609948fb96
Yuri Iozzelli <y.iozzelli@gmail.com>
2020-07-20 10:25:09 +0200

    Add a ConstantExprLowering pass to convert ConstantExpr to Instruction

--- include/llvm/InitializePasses.h
+++ include/llvm/InitializePasses.h
@@ -440,6 +440,7 @@ void initializeCheerpLowerSwitchPass(PassRegistry&);
 void initializeFixFunctionCastsPass(PassRegistry&);
 void initializeByValLoweringPass(PassRegistry&);
 void initializeI64LoweringPass(PassRegistry&);
+void initializeConstantExprLoweringPass(PassRegistry&);
 
 } // end namespace llvm
 
