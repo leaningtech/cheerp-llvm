2986d5f1303d472d385c492617f1863f106b6e31
Yuri Iozzelli <y.iozzelli@gmail.com>
2020-07-21 15:54:19 +0200

    I64Lowering: Expose the functionality as a standalone class (not a Pass)
    The actual Pass is called I64LoweringPass

--- include/llvm/InitializePasses.h
+++ include/llvm/InitializePasses.h
@@ -439,7 +439,7 @@ void initializeFixIrreducibleControlFlowPass(PassRegistry&);
 void initializeCheerpLowerSwitchPass(PassRegistry&);
 void initializeFixFunctionCastsPass(PassRegistry&);
 void initializeByValLoweringPass(PassRegistry&);
-void initializeI64LoweringPass(PassRegistry&);
+void initializeI64LoweringPassPass(PassRegistry&);
 void initializeConstantExprLoweringPass(PassRegistry&);
 
 } // end namespace llvm
